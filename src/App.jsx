cef4b9b5e5ba393c938b971de82d3d8eeaaa7188
Win Luyeye <Winnerluyeye1@gmail.com>
2024-12-19 05:15:05 +0100

    Initalisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Elevata 243 </h1>
+    </div>
+  )
+}
+
+export default App
